04c8e58c2880fca1a08c59ffdfdd9bc6644a3eb3
Favour Ikechukwu <149949474+Favour-Ikec@users.noreply.github.com>
2024-12-02 16:12:44 +0100

    This is for the footer section. It is inside it's own folder
    Footer Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import ClimbingDisciplines from './components/ClimbingDisciplines/ClimbingDisciplines';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
         <ClimbingDisciplines />
+        <Footer />
     </div>
   );
 }
