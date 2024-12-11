dde8a74648b39430893928ab2364389ae314c0a3
Favour Ikechukwu <149949474+Favour-Ikec@users.noreply.github.com>
2024-12-11 12:41:54 +0100

    Header, Footer, and Edits
    Edits for Footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@
 import './App.css';
 import ClimbingDisciplines from './components/ClimbingDisciplines/ClimbingDisciplines';
 import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Sheltering from './components/Sheltering/Sheltering';
+
 
 function App() {
   return (
     <div className="App">
+        <Header />
+         <Sheltering />
         <ClimbingDisciplines />
         <Footer />
+       
     </div>
   );
 }
